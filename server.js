a1461bf9fb696e06bcd434757397fdde5dc0aa18
mateusz <mateuszalmannai@googlemail.com>
2020-06-21 13:03:23 +0200

    lookup environment variable from javascript

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// Set a local variable populated by the environment variable $NAME
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while (true) {
-    console.log('Containers rule!');
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
 }
